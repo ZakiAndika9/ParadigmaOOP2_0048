9840791c2e75434a71b08c57d58f4a75008c5d8b
ZakiAndika9 <m.zaki.andika9@gmail.com>
2025-06-02 11:29:24 +0700

    Add derivedClass attempting to override final method perkenalan

--- noOverriding/noOverriding.cpp
+++ noOverriding/noOverriding.cpp
@@ -7,3 +7,16 @@ class baseClass {
 		cout << "Hello saya Function dari base Class";
 	}
 };
+class derivedClass : public baseClass {
+	public:
+	void perkenalan(){
+		cout << "Hello saya Function dari derived Class";
+	}
+};
+
+int main(){
+	derivedClass a;
+
+	a.perkenalan();
+	return 0;
+}
