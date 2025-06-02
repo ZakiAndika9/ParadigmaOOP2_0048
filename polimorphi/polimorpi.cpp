5b0ed35673edb44133c143fc84466aa9ab2b3e87
ZakiAndika9 <m.zaki.andika9@gmail.com>
2025-06-02 11:34:35 +0700

    Add abstract seseorang class with pure virtual function pesan

---
+++ polimorphi/polimorpi.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class seseorang {
+	public:
+	virtual void pesan() = 0;
+};
+
+class joko : public seseorang {
+	public:
+	void pesan (){
+		cout << "Pesan dari joko" << endl;
+	}
+};
+
